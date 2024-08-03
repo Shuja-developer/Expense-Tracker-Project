053061f235a2664e8137ba885ae58ce940f7f163
Shuja-developer <khansajaullah793@gmail.com>
2024-08-03 20:20:12 +0500

    file

---
+++ Expense Tracker/Controllers/TransactionsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Expense_Tracker.Models;
+
+namespace Expense_Tracker.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TransactionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transactions
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.transactions.Include(t => t.Category);
+            return View(await applicationDbContext.ToListAsync());
+        }
+        // GET: Transactions/AddOrEdit
+        public IActionResult AddOrEdit()
+        {
+            PopulateCategories();
+            return View(new Transaction());
+        }
+
+        // POST: Transactions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit(Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(transaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+             PopulateCategories();
+             return View(transaction);
+        }
+
+        // POST: Transactions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.transactions == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.transactions'  is null.");
+            }
+            var transaction = await _context.transactions.FindAsync(id);
+            if (transaction != null)
+            {
+                _context.transactions.Remove(transaction);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        [NonAction]
+        public void PopulateCategories()
+        {
+            var categoryCollection = _context.Categories.ToList();
+            Category DefaultCategory = new Category() { CategoryId = 0, Title = "Choose a Category" };
+            categoryCollection.Insert(0, DefaultCategory);
+            ViewBag.Categories = categoryCollection;
+        }
+    }
+}
